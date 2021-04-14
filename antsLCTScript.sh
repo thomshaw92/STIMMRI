c83fc1622dc159a01d328089fa9a1d1d15a5f609
thomshaw92 <thomasbasilshaw@gmail.com>
2021-04-14 16:50:50 +1000

    Script updates - now working

--- antsLCTScript.sh
+++ antsLCTScript.sh
@@ -7,15 +7,20 @@
 #Will run on HPCs - PBS
 
 subjName=$1
-singularity="singularity exec --bind /30days:/30days/ /90days/uqtshaw/ants_2.3.4.sif"
+singularity="singularity exec --bind /30days:/30days/ /home/uqtshaw/ants_2.3.4.sif"
 module load singularity
 mkdir -p /30days/$USER/STIMMRI/alct/$subjName
 cd /30days/$USER/STIMMRI/alct/$subjName
 
+data_dir="/30days/uqtshaw/STIMMRI_BIDS"
 atlas_dir="/30days/uqtshaw/STIMMRI_ATLAS"
 
+tp_1_t1w=$(echo "${data_dir}/${subjName}/ses-01/anat/${subjName}"*"T1w.nii.gz")
+tp_2_t1w=$(echo "${data_dir}/${subjName}/ses-02/anat/${subjName}"*"T1w.nii.gz")
+tp_3_t1w=$(echo "${data_dir}/${subjName}/ses-03/anat/${subjName}"*"T1w.nii.gz")
+
 #ANTS LCT 3TP
-antsLongitudinalCorticalThickness.sh -d 3 \
+${singularity} antsLongitudinalCorticalThickness.sh -d 3 \
 -e ${atlas_dir}/STIMMRI_T1w_template0.nii.gz \
 -m ${atlas_dir}/antsCTBrainExtractionMaskProbabilityMask.nii.gz \
 -p ${atlas_dir}/antsCTBrainSegmentationPosteriors%d.nii.gz \
@@ -27,16 +32,12 @@ antsLongitudinalCorticalThickness.sh -d 3 \
 -j '10' \
 -r '1' \
 -q '0' \
--n '1' \ 
+-n '1' \
 -b '1' \
-${subjName}_ses-01_T1w_N4corrected_norm_preproc.nii.gz ${subjName}_ses-01_T2w_NlinMoCo_res-iso.3_N4corrected_denoised_brain_preproc.nii.gz \
-${subjName}_ses-06_T1w_N4corrected_norm_preproc.nii.gz ${subjName}_ses-06_T2w_NlinMoCo_res-iso.3_N4corrected_denoised_brain_preproc.nii.gz \
-${subjName}_ses-12_T1w_N4corrected_norm_preproc.nii.gz ${subjName}_ses-12_T2w_NlinMoCo_res-iso.3_N4corrected_denoised_brain_preproc.nii.gz
+${tp_1_t1w} \
+${tp_2_t1w} \
+${tp_3_t1w}
 
-cd $TMPDIR/$subjName/
-tar -zcvf ./${subjName}lct_3_timepoints.tar.gz ./${subjName}lct_3_timepoints ./${subjName}lct_3_timepointsSingleSubjectTemplate
-if [[ ! -e $TMPDIR/$subjName/${subjName}lct_3_timepoints.tar.gz ]] ; then
-    echo "${subjName} 3TP ANTSLCT didn't work.">>/RDS/Q0535/optimex/data/derivatives/ALCT_errorList.txt
-fi
-mkdir /RDS/Q0535/optimex/data/derivatives/3_alct/$subjName
-rsync -v -r $TMPDIR/$subjName/${subjName}lct_3_timepoints.tar.gz /RDS/Q0535/optimex/data/derivatives/3_alct/$subjName/
+
+#JLF the data
+
