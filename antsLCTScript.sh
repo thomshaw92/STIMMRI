2200198d5ac4211054c02fbbee5c74d4db318e9a
thomshaw92 <thomasbasilshaw@gmail.com>
2021-04-20 15:13:40 +1000

    Changes to fix typos

--- antsLCTScript.sh
+++ antsLCTScript.sh
@@ -7,8 +7,8 @@
 #Will run on HPCs - PBS
 
 subjName=$1
-singularity="singularity exec --bind /30days:/30days/ /home/uqtshaw/ants_2.3.4.sif"
-module load singularity
+#singularity="singularity exec --bind /30days:/30days/ /home/uqtshaw/ants_2.3.4.sif"
+#module load singularity
 mkdir -p /30days/$USER/STIMMRI/alct/$subjName
 cd /30days/$USER/STIMMRI/alct/$subjName
 
@@ -20,23 +20,21 @@ tp_2_t1w=$(echo "${data_dir}/${subjName}/ses-02/anat/${subjName}"*"T1w.nii.gz")
 tp_3_t1w=$(echo "${data_dir}/${subjName}/ses-03/anat/${subjName}"*"T1w.nii.gz")
 
 #ANTS LCT 3TP
-${singularity} antsLongitudinalCorticalThickness.sh -d 3 \
+antsLongitudinalCorticalThickness.sh -d 3 \
 -e ${atlas_dir}/STIMMRI_T1w_template0.nii.gz \
 -m ${atlas_dir}/antsCTBrainExtractionMaskProbabilityMask.nii.gz \
 -p ${atlas_dir}/antsCTBrainSegmentationPosteriors%d.nii.gz \
 -f ${atlas_dir}/antsCTBrainExtractionMask.nii.gz \
 -t ${atlas_dir}/antsCTBrainExtractionBrain.nii.gz \
 -o ${subjName}_long_cortical_thickness \
--k '2' \
+-k '1' \
 -c '2' \
 -j '10' \
 -r '1' \
 -q '0' \
 -n '1' \
 -b '1' \
-${tp_1_t1w} \
-${tp_2_t1w} \
-${tp_3_t1w}
+${tp_1_t1w} ${tp_2_t1w} ${tp_3_t1w}
 
 
 #JLF the data
