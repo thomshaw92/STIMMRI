f23602cdb4ac15921f9a94ae4c143ecc7c503964
thomshaw92 <thomasbasilshaw@gmail.com>
2021-04-21 13:49:56 +1000

    Now with JLF!

--- antsLCTScript.sh
+++ antsLCTScript.sh
@@ -14,28 +14,44 @@ cd /30days/$USER/STIMMRI/alct/$subjName
 
 data_dir="/30days/uqtshaw/STIMMRI_BIDS"
 atlas_dir="/30days/uqtshaw/STIMMRI_ATLAS"
+out_dir="/30days/$USER/STIMMRI/alct/$subjName/${subjName}_long_cortical_thickness"
 
 tp_1_t1w=$(echo "${data_dir}/${subjName}/ses-01/anat/${subjName}"*"T1w.nii.gz")
 tp_2_t1w=$(echo "${data_dir}/${subjName}/ses-02/anat/${subjName}"*"T1w.nii.gz")
 tp_3_t1w=$(echo "${data_dir}/${subjName}/ses-03/anat/${subjName}"*"T1w.nii.gz")
 
 #ANTS LCT 3TP
-antsLongitudinalCorticalThickness.sh -d 3 \
--e ${atlas_dir}/STIMMRI_T1w_template0.nii.gz \
--m ${atlas_dir}/antsCTBrainExtractionMaskProbabilityMask.nii.gz \
--p ${atlas_dir}/antsCTBrainSegmentationPosteriors%d.nii.gz \
--f ${atlas_dir}/antsCTBrainExtractionMask.nii.gz \
--t ${atlas_dir}/antsCTBrainExtractionBrain.nii.gz \
--o ${subjName}_long_cortical_thickness \
--k '1' \
--c '2' \
--j '10' \
--r '1' \
--q '0' \
--n '1' \
--b '1' \
-${tp_1_t1w} ${tp_2_t1w} ${tp_3_t1w}
-
+if [[ ! -d /30days/uqtshaw/STIMMRI/alct/${subjName}/${subjName}_long_cortical_thickness ]] ; then
+    antsLongitudinalCorticalThickness.sh -d 3 \
+    -e ${atlas_dir}/STIMMRI_T1w_template0.nii.gz \
+    -m ${atlas_dir}/antsCTBrainExtractionMaskProbabilityMask.nii.gz \
+    -p ${atlas_dir}/antsCTBrainSegmentationPosteriors%d.nii.gz \
+    -f ${atlas_dir}/antsCTBrainExtractionMask.nii.gz \
+    -t ${atlas_dir}/antsCTBrainExtractionBrain.nii.gz \
+    -o ${subjName}_long_cortical_thickness \
+    -k '1' \
+    -c '2' \
+    -j '10' \
+    -r '1' \
+    -q '0' \
+    -n '1' \
+    -b '1' \
+    ${tp_1_t1w} ${tp_2_t1w} ${tp_3_t1w}
+fi
 
 #JLF the data
 
+for TP in 01 02 03 ; do 
+
+atlasDir=/30days/uqtshaw/mindboggle_all_data
+target_image=$(echo "${output_dir}/${subjName}_ses-${TP}_"*"/${subjName}_ses-${TP}_"*"T1wExtractedBrain0N4.nii.gz")
+
+command="antsJointLabelFusion.sh -d 3 -t ${target_image} -x or -o dkt -c 2 -j 14" 
+
+for i in {1..20}; 
+  do
+    command="${command} -g ${atlasDir}/OASIS-TRT-20_volumes/OASIS-TRT-20-${i}/t1weighted_brain.nii.gz"
+    command="${command} -l ${atlasDir}/OASIS-TRT-20_DKT31_CMA_labels_v2/OASIS-TRT-20-${i}_DKT31_CMA_labels.nii.gz"
+  done
+ 
+ $command
