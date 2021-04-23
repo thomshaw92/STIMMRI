19efb6bdd72d982bedf682cac978bf4506f74aa7
thomshaw92 <thomasbasilshaw@gmail.com>
2021-04-23 10:15:58 +1000

    fixes to make it easier to debug

--- unzip_files_for_bidscoiner.sh
+++ unzip_files_for_bidscoiner.sh
@@ -7,10 +7,11 @@ raw_dir="/path/to/30days/raw_data_dir"
 #start unzipping
 for x in {001..250}  ; do #or however many participants you have in raw_dir
     for ses in 01 02 03 ; do
+        echo "doing subject ${x} in session ${ses}"
         if [[ -d ${raw_dir}/sub-${x}/ses-${ses} ]] ; then
             cd ${raw_dir}/sub-${x}/ses-${ses}
             for file in *zip ; do
-                unzip ${file}
+                unzip ${file} -d ${file:0:-4}
                 rm -rf ${file}
             done
         fi
