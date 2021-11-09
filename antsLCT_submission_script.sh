f32d376473461a12179e6ed8d209a4fad613ef94
thomshaw92 <thomasbasilshaw@gmail.com>
2021-11-09 14:58:02 +1000

    submission script update for slurm

--- antsLCT_submission_script.sh
+++ antsLCT_submission_script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-for subjName in `cat ~/scripts/STIMMRI/subjnames.csv` ; do 
-	qsub -v SUBJNAME=$subjName ~/scripts/STIMMRI/antsLCT_PBS_script.pbs
+#for subjName in `cat ~/scripts/STIMMRI/subjnames.csv` ; do 
+#	qsub -v SUBJNAME=$subjName ~/scripts/STIMMRI/antsLCT_PBS_script.pbs
+#done
+#SLURM
+for subjName in `cat /data/lfs2/${USER}/STIMMRI/subjnames.csv` ; do 
+    sbatch --export=SUBJNAME=$subjName /data/lfs2/${USER}/STIMMRI/anstsLCT_SLURM.sh
 done
