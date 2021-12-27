81433cd21326aef562ed3f23060cc8bee0bfcf8c
thomshaw92 <thomasbasilshaw@gmail.com>
2021-12-27 13:34:15 +1000

    changes to serial scripts

--- antsLCTScript_serial.sh
+++ antsLCTScript_serial.sh
@@ -100,25 +100,25 @@ for subjName in `cat /30days/uqtshaw/STIMMRI/scripts/STIMMRI/subjnames.csv` ; do
 		$command
 	    fi
 	fi
-    
+	
 
-ml fsl
-#collect some stats
-for num in 1003 1008 1012 1014 1018 1019 1020 1024 1027 1028 2003 2008 2012 2014 2018 2019 2020 2024 2027 2028 ; do
-    labelledimage=$(echo "${outdir_JLF}/dkt_"*"Labels.nii.gz")
-    fslmaths ${labelledimage} -thr ${num} -uthr ${num} ${outdir_JLF}/${num}_mask.nii.gz
-    fslmaths ${outdir_JLF}/${num}_mask.nii.gz -bin ${outdir_JLF}/${num}_mask.nii.gz
-    cortical_thickness_image=$(echo "${out_dir}/${subjName}_ses-${TP}_"*"/${subjName}_ses-${TP}*_run-1_T1wCorticalThickness.nii.gz")
-    echo "${subjName} ses-${TP} ${num}">> ${outdir_JLF}/numstats.csv
-    #the stats are going to be min intensity, max intensity, mean of nonzero values, SD of nonzeroes
-    fslstats ${cortical_thickness_image} -k ${outdir_JLF}/${num}_mask.nii.gz -l 0.01 -R -M -S >> ${outdir_JLF}/stats.csv
-    #LabelGeometryMeasures 3  ${outdir_JLF}/${num}_mask.nii.gz [intensityImage=none] ${outdir_JLF}/stats_ants.csv
-    paste -d' ' ${outdir_JLF}/numstats.csv ${outdir_JLF}/stats.csv > ${outdir_JLF}/statsfile_temp.csv
-    cat ${outdir_JLF}/statsfile_temp.csv >> ${scratch}/STIMMRI/alct/statsfile.csv
-    tr -s '\t' <${scratch}/STIMMRI/alct/statsfile.csv | tr '\t' ',' >${scratch}/STIMMRI/alct/statsfilenowhite.csv
-    #rm ${scratch}/STIMMRI/alct/statsfile.csv
-    #mv ${scratch}/STIMMRI/alct/statsfilenowhite.csv ${scratch}/STIMMRI/alct/statsfile.csv
-    rm ${outdir_JLF}/statsfile_temp.csv ${outdir_JLF}/numstats.csv ${outdir_JLF}/stats.csv
-done
-done 
+	ml fsl
+	#collect some stats
+	for num in 1003 1008 1012 1014 1018 1019 1020 1024 1027 1028 2003 2008 2012 2014 2018 2019 2020 2024 2027 2028 ; do
+	    labelledimage=$(echo "${outdir_JLF}/dkt_${TP}Labels.nii.gz")
+	    fslmaths ${labelledimage} -thr ${num} -uthr ${num} ${outdir_JLF}/${num}_mask.nii.gz
+	    fslmaths ${outdir_JLF}/${num}_mask.nii.gz -bin ${outdir_JLF}/${num}_mask.nii.gz
+	    cortical_thickness_image=$(echo "${out_dir}/${subjName}_ses-${TP}_"*"/${subjName}_ses-${TP}*_run-1_T1wCorticalThickness.nii.gz")
+	    echo "${subjName} ses-${TP} ${num}">> ${outdir_JLF}/numstats.csv
+	    #the stats are going to be min intensity, max intensity, mean of nonzero values, SD of nonzeroes
+	    fslstats ${cortical_thickness_image} -k ${outdir_JLF}/${num}_mask.nii.gz -l 0.01 -R -M -S >> ${outdir_JLF}/stats.csv
+	    #LabelGeometryMeasures 3  ${outdir_JLF}/${num}_mask.nii.gz [intensityImage=none] ${outdir_JLF}/stats_ants.csv
+	    paste -d' ' ${outdir_JLF}/numstats.csv ${outdir_JLF}/stats.csv > ${outdir_JLF}/statsfile_temp.csv
+	    cat ${outdir_JLF}/statsfile_temp.csv >> ${scratch}/STIMMRI/alct/statsfile.csv
+	    tr -s '\t' <${scratch}/STIMMRI/alct/statsfile.csv | tr '\t' ',' >${scratch}/STIMMRI/alct/statsfilenowhite.csv
+	    #rm ${scratch}/STIMMRI/alct/statsfile.csv
+	    #mv ${scratch}/STIMMRI/alct/statsfilenowhite.csv ${scratch}/STIMMRI/alct/statsfile.csv
+	    rm ${outdir_JLF}/statsfile_temp.csv ${outdir_JLF}/numstats.csv ${outdir_JLF}/stats.csv
+	done
+    done 
 done
